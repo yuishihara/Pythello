7b1e8104d6aaffe5dce3a03118445c41048c2e82
Yu Ishihara <yuishihara1225@gmail.com>
2018-05-01 15:13:10 +0900

    Add extract board interface

--- othello/board.py
+++ othello/board.py
@@ -1,6 +1,35 @@
 import numpy as np
+
+
 class Board(object):
     def __init__(self):
+        pass
+
+    def is_valid_move(self, move, color_number):
+        pass
+
+    def apply_new_move(self, move, player_color):
+        pass
+
+    def has_valid_move(self, color_number):
+        pass
+
+    def is_end_state(self):
+        pass
+
+    def is_out_of_board(self, position):
+        pass
+
+    def is_empty_position(self, position):
+        pass
+
+    def list_all_valid_moves(self, color_number):
+        pass
+
+    def list_all_empty_positions(self):
+        pass
+
+    def next_board_state(self, move, player_color):
         pass
 
     def as_numpy_matrix(self):
