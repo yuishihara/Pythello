e3ed86793889c44cbc85c372091d0f485654d5b9
Yu Ishihara <yuishihara1225@gmail.com>
2018-05-03 16:05:43 +0900

    Add stone count method to utility

--- othello/utilities.py
+++ othello/utilities.py
@@ -3,3 +3,12 @@ import numpy as np
 
 def color_string_to_number(color_string):
     return (-1 if (color_string == 'black') else 1)
+
+
+def count_stone_num(board_state):
+    matrix = board_state.as_numpy_matrix()
+    black = np.count_nonzero(
+        matrix == color_string_to_number('black'))
+    white = np.count_nonzero(
+        matrix == color_string_to_number('white'))
+    return (black, white)
